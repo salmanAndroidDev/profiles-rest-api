fd1d2b5284c8f7c5a664e4a5f62dca4cc8ad0bae
Salman Barani <salmanAndroidDev@yahoo.com>
2020-06-05 20:41:31 +0430

    added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from profiles_api.views import HelloApiView, HelloViewSet, UserProfileViewSet
+from profiles_api.views import HelloApiView, HelloViewSet, UserProfileViewSet, UserLoginApiView
 from rest_framework.routers import DefaultRouter
 
 
@@ -8,5 +8,6 @@ router.register('hello-viewset', HelloViewSet, base_name= 'Hello-viewset')
 router.register('profile', UserProfileViewSet)
 urlpatterns = [
     path('hello-view/', HelloApiView.as_view()),
+    path('login/', UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from profiles_api import serializers, models, permissions
 from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class HelloApiView(APIView):
     """Test API View """
@@ -98,3 +100,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle users authentication tokens"""
+    renderer_classes= api_settings.DEFAULT_RENDERER_CLASSES
+    
